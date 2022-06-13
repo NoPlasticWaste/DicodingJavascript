14e5f50309b4d8ae7e25bec383687f39e5e4fea1
Vivien <endarlan11@gmail.com>
2022-06-13 20:46:48 +0700

    latihan If isRaining

--- Dicoding Js.js
+++ Dicoding Js.js
@@ -1,7 +1,14 @@
-console.log("Hello, world!");
+//console.log("Hello, world!");
 
 // It's a comment.
 
 /* It's multiple
 line comments. */
 
+const isRaining = true;
+
+console.log("Persiapan sebelum berangkat kegiatan.");
+if (isRaining) {
+    console.log("Hari ini hujan. Bawa payung.");
+}
+console.log("Berangkat kegiatan.");
